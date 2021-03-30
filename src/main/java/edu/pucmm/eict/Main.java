a1b4268ff7bc1d1c77d2a67aa294d1cc9be027e1
Wendily <wendilyrr@gmail.com>
2021-03-30 02:48:46 -0400

    Initial commit

---
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -0,0 +1,23 @@
+package edu.pucmm.eict;
+
+import io.javalin.Javalin;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        Javalin app = Javalin.create(config -> {
+           // config.addStaticFiles("/publico"); //desde la carpeta de resources --> Ruta estatica.
+        }).start(7000);
+
+        System.out.println("Javalin");
+
+        app.get("/", ctx -> {
+           ctx.result("Bienvenido a aplicacion 1");
+        });
+
+
+    }
+}
